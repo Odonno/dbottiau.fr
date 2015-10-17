01ae1e7f2047a4b251f9104df2d3d0a9c08add8a
Odonno <bottiau.david@laposte.net>
2015-10-17 18:01:34 +0200

    chore(app): move ghost blog to root

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://http://dbottiau.azurewebsites.net.com',
+        url: 'http://dbottiau.azurewebsites.net',
         mail: {},
         database: {
             client: 'sqlite3',
